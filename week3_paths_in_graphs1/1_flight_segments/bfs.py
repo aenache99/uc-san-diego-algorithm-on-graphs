534f48620e4cdb3c7fb0fefee571c4d757e3c25a
aenache99 <alexenache71@gmail.com>
2023-09-05 11:06:21 +0300

    wk3 done

--- week3_paths_in_graphs1/1_flight_segments/bfs.py
+++ week3_paths_in_graphs1/1_flight_segments/bfs.py
@@ -1,15 +1,33 @@
-#Uses python3
-
 import sys
-import queue
+from collections import deque
+
 
 def distance(adj, s, t):
-    #write your code here
-    return -1
+    # Initialize an array to keep track of visited vertices
+    visited = [False] * len(adj)
+    # Initialize a queue for BFS
+    queue = deque()
+    # Start from vertex s
+    queue.append(s)
+    visited[s] = True
+    distance = [-1] * len(adj)  # Initialize distances to -1
+    distance[s] = 0  # The distance from s to itself is 0
+
+    while queue:
+        vertex = queue.popleft()
+
+        for neighbor in adj[vertex]:
+            if not visited[neighbor]:
+                queue.append(neighbor)
+                visited[neighbor] = True
+                distance[neighbor] = distance[vertex] + 1
+
+    return distance[t]
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    input_data = sys.stdin.read()
+    data = list(map(int, input_data.split()))
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
--- week3_paths_in_graphs1/2_bipartite/bipartite.py
+++ week3_paths_in_graphs1/2_bipartite/bipartite.py
@@ -1,15 +1,33 @@
-#Uses python3
-
 import sys
-import queue
+from queue import Queue
+
 
 def bipartite(adj):
-    #write your code here
-    return -1
+    n = len(adj)
+    colors = [-1] * n  # Initialize colors to -1 (unvisited)
+
+    for start_vertex in range(n):
+        if colors[start_vertex] == -1:
+            colors[start_vertex] = 0  # Assign color 0 to the start vertex
+            queue = Queue()
+            queue.put(start_vertex)
+
+            while not queue.empty():
+                vertex = queue.get()
+
+                for neighbor in adj[vertex]:
+                    if colors[neighbor] == -1:
+                        colors[neighbor] = 1 - colors[vertex]  # Assign the opposite color
+                        queue.put(neighbor)
+                    elif colors[neighbor] == colors[vertex]:
+                        return 0  # Not bipartite, adjacent vertices have the same color
+
+    return 1  # Bipartite
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    input_data = sys.stdin.read()
+    data = list(map(int, input_data.split()))
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
